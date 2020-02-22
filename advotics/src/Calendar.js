9f2fd4645a6bf6f3cbac20e50fee739eac86f0c3
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2020-02-22 17:01:07 +0700

    filter not done, insights average purchase chart not done

---
+++ advotics/src/Calendar.js
@@ -0,0 +1,28 @@
+// import React, {Component} from 'react';
+// import DateRangePicker from 'react-date-range';
+// import { addDays } from 'date-fns';
+// import { useState } from 'react';
+// import 'react-date-range/dist/styles.css';
+// import 'react-date-range/dist/theme/default.css';
+
+
+// export default function Calendar () {
+//     const [state, setState] = useState([
+//         {
+//           startDate: new Date(),
+//           endDate: addDays(new Date(), 7),
+//           key: 'selection'
+//         }
+//       ]);
+//         return(
+//             // <DateRangePicker 
+//             //     onChange={item => setState([item.selection])}
+//             //     showSelectionPreview={true}
+//             //     moveRangeOnFirstSelection={false}
+//             //     months={2}
+//             //     ranges={state}
+//             //     direction="horizontal"
+//             // />
+//         )
+    
+// }
--- advotics/src/Filter.js
+++ advotics/src/Filter.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import calendar from './calendar@2x.png';
+import calendar2 from './calendar.png';
 import arrow from './down-arrow.png';
 import './App.css';
+import Calendar from './Calendar.js';
+
 
 class Filter extends React.Component {
     state = {show : true};
+
     render(){
         const {show } = this.state;
+        
         return (
            <div className = "filter">
                         <img src={calendar} className="calendar-icon"/>
                         <p className="period-title">Period</p>
-                        <p className="period">11 September 2018 - 14 September 2018</p>
+                        <p id="period">11 September 2018 - 14 September 2018</p>
                         <img src={arrow} className="arrow-down" onClick={() => this.setState({show:!show})} />
                         {show
-                        ?<div className="filter-date-picker"> 
-                            "Hello"
+                        ?
+                        <div className="filter-date-picker"> 
+                            <div className="period-range">
+                               <img src={calendar2} className="calendar-icon range"/>                     
+                               <p className="period-title range">Period</p>
+                                {/* <Calendar></Calendar> */}
+                            </div>
                         </div>
                         : null
                         }
